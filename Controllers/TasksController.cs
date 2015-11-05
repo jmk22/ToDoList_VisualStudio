2bb76c33485fda14d5b2bb07a36ce05335ff7d9e
Jill Kuchman <jillkuchman@gmail.com>
2015-11-05 15:21:26 -0800

    Add initial files up to Create page

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TasksController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Tasks/Details/{id}
+        public ActionResult Details (int? id)
+        {
+            return View();
+        }
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tasks/Create
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            db.Tasks.Add(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Tasks/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Tasks/Edit/{id}
+        [HttpPost]
+        public ActionResult Edit(Task task)
+        {
+            return View();
+        }
+
+        // GET: Tasks/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        // POST : Tasks/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=ToDoListContext")
+        {
+
+        }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
